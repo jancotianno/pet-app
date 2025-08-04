36ee74782445267a9591209bfaf342dd4e4cbf76
matteo.zmyslowski <matteo.zmyslowski@linksmt.it>
2025-08-04 18:11:17 +0200

    Initial commit

---
+++ src/main/java/com/example/petapp/PetappApplication.java
@@ -0,0 +1,13 @@
+package com.example.petapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petapp/PetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
